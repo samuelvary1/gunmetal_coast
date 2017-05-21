fb47578a2b2ba4ea00efc577012d3b5a1e543417
Sam Vary <sam.vary@gmail.com>
2017-05-21 11:14:20 -0400

    some p tags and fixed a spelling error on the 'splash' page

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,9 @@
-The cowboy’s only safe place is in his mind.
 
-The gunslinger looked out over the prairie beyond the front railing of the saloon. The breeze that sent a few tumbleweeds to rustle drily along felt like the breath of the devil himself. From inside came the sounds of revelry, glasses clinking and bursts of female laughter. Wouldn’t be but a few moments now before the sun began dipping its way below the horizon, and the plains beyond the edge of town, and the cliffs overlooking the ocean just beyond that, would be awash in blood-red light. 
 
-The men who had promised to kill him would be here soon. That much was certain. At best he would have a few days to prepare, to learn what we could about this town, to gather items that might help him in his quest to stay alive. Oh, they'd be along shortly, there was no doubt about that. A woman emerged from the mileu behind him to slip a hand around his waist, inquiring if he might like some company for the evening.
+<p><i>The cowboy’s only safe place is in his mind.</i></p>
 
-Welcome to Gunmetal Coast, he thought.
+<p>The gunslinger looked out over the prairie beyond the front railing of the saloon. The breeze that sent a few tumbleweeds to rustle drily along felt like the breath of the devil himself. From inside came the sounds of revelry, glasses clinking and bursts of female laughter. Wouldn’t be but a few moments now before the sun began dipping its way below the horizon, and the plains beyond the edge of town, and the cliffs overlooking the ocean just beyond that, would be awash in blood-red light.</p> 
+
+<p>The men who had promised to kill him would be here soon. That much was certain. At best he would have a few days to prepare, to learn what we could about this town, to gather items that might help him in his quest to stay alive. Oh, they'd be along shortly, there was no doubt about that. A woman emerged from the milieu behind him to slip a hand around his waist, inquiring if he might like some company for the evening.</p>
+
+<p>Welcome to Gunmetal Coast, he thought.</p>
