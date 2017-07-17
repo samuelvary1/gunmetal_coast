84c2214573fd1a35fd4a155201a3d4e418ce0514
samuelvary1 <svary@triplelift.com>
2017-07-17 16:05:32 -0400

    adjusting the bidders slightly

--- application/views/pages/prebid.php
+++ application/views/pages/prebid.php
@@ -5,34 +5,38 @@
   <!-- Prebid Config Section START -->
   <!-- Make sure this is inserted before your GPT tag -->
   <script> 
-    var PREBID_TIMEOUT = 700;
+    var PREBID_TIMEOUT = 10000;
 
     var adUnits = [{
         code: 'div-gpt-ad-1460505748561-0',
         sizes: [[300, 250], [300,600]],
-        bids: [{
-            bidder: 'appnexus',
-            params: {
-               placementId: '10433394'
-            }
-        },{
+        bids: [
+        // {
+        //     bidder: 'appnexus',
+        //     params: {
+        //        placementId: '10433394'
+        //     }
+        // },
+        {
             bidder: 'triplelift',
             params: {
-               inventoryCode: '[INVENTORY_CODE]'
+               inventoryCode: 'sams_literary_novelties_300x250'
             }
         }]
     },{
         code: 'div-gpt-ad-1460505661639-0',
         sizes: [[728, 90], [970, 90]],
-        bids: [{
-            bidder: 'appnexus',
-            params: {
-               placementId: '10433394'
-            }
-        },{
+        bids: [
+        // {
+        //     bidder: 'appnexus',
+        //     params: {
+        //        placementId: '10433394'
+        //     }
+        // },
+        {
             bidder: 'triplelift',
             params: {
-               inventoryCode: '[INVENTORY_CODE]'
+               inventoryCode: 'sams_literary_novelties_728x90'
             }
         }]
     }];
