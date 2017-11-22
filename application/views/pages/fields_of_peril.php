4c970f18f17e90e89093e662051254fa47fda443
Sam Vary <sam.vary@gmail.com>
2017-11-22 14:55:45 -0500

    attempt to add a new controller and model for location and connect to db

---
+++ application/views/pages/fields_of_peril.php
@@ -0,0 +1,30 @@
+<p>Fields of Peril</p>
+
+
+<button id="submit_command" class="btn-primary">Submit Command</button>
+
+<script>
+
+	$('#set_budget').click(save_budget_setting);
+	
+	function get_all_locations() {
+		do_get("location/get_all_locations",
+			function(data) {
+					debugger;
+				}
+		});
+	}
+
+	function do_get(url, callback) {
+    jQuery.ajax({
+      type: 'GET',
+      url: api_endpoint + url,
+      error: do_api_error,
+      success: callback,
+      dataType: 'json',
+      contentType: "application/json"
+      // headers: {"Auth-token": auth_token, "Accept": "application/json"}
+    });
+	}
+
+</script>
