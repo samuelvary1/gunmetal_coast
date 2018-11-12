dd6aaf78b121837db1aa292ab146816acc21ed1b
samuelvary1 <svary@triplelift.com>
2018-11-12 15:36:08 -0500

    adding a new test page

---
+++ application/views/pages/test_page_1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Generic Test Page 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="prebid.js" async></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+    <div class="container">
+<!--     	<p><i>The cowboy’s only safe place is in his mind....</i></p>
+    	<p>The gunslinger looked out over the prairie beyond the front railing of the saloon. The breeze that sent a few tumbleweeds to rustle drily along felt like the breath of the devil himself. From inside came the sounds of revelry, glasses clinking and bursts of female laughter. Wouldn’t be but a few moments now before the sun began dipping its way below the horizon, and the plains beyond the edge of town, and the cliffs overlooking the ocean just beyond that, would be awash in blood-red light.</p> 
+    	<p>The henchmen who had promised to kill him would be here soon. That much was certain. At best he would have a few days to prepare, to learn what we could about this town, to gather items that might help him in his quest to stay alive. Oh, they'd be along shortly, there was no doubt about that. A woman emerged from the milieu behind him to slip a hand around his waist, inquiring if he might like some company for the evening.</p>
+    	<p>Welcome to Gunmetal Coast, he thought.</p> -->
+    </div>
+
+<!--     <div class="container" style="margin-top:20px">
+      <p style="font-weight: bold">Enter Command: </p><input type="text" name="command">
+      <input type="submit" value="Submit" class="btn-primary">
+    </div> -->
+
+  </body>
+</html>
