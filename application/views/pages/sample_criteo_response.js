8fd41c903be7fc82d2a2e2eab9ffe3c5d57b35c9
samuelvary1 <svary@triplelift.com>
2018-12-17 17:18:17 -0500

    trying to host a DCR for Strickland

---
+++ application/views/pages/sample_criteo_response.js
@@ -0,0 +1,29 @@
+dynamicCreativeRender({
+  "settings": {
+    "advertiser_name": "Accor Hotels",
+    "viewability": {},
+    "type": "image",
+    "additional_data": {
+      "brid": "3846",
+      "bmid": "2711",
+      "aid": "163343272884323596890",
+      "did": "2798",
+      "ts": "1525274523",
+      "sid": "7358"
+    },
+    "logo_image_url": "https://pix.as.criteo.net/img/img?h=200&i=10778-&m=0&p=1&q=100&r=0&u=http%3A%2F%2Fstatic.hk.as.criteo.net%2Fdesign%2Fdt%2F2172%2F150619%2F1025806.jpg&v=2&w=200&s=hKY0gHj-d8OOIHegylu-o8V1",
+    "adchoices_url": "https://info.criteo.com/privacy/informations?infonorm=3&partner=10778&campaignid=99337&zoneid=416433&bannerid=6614452&displayid=5ae9d79a0afe4c36ca567f6dfca44e00&uaCap=0&aid=Cb0O3nxHaVMzWURYd2ZxUUp5NHc4WGpFVDlTbFhORURueVhTZVV4Z2xhNGpXZEtJPXw=&u=%7Cj3sjhpcj%2BO%2B3wys1PKj731r%2FegNc2S8synZgdNyiRsA%3D%7C",
+    "client_side_render": true,
+    "format_id": 1,
+    "render_options_bm": 8
+  },
+  "assets": [
+    {
+      "image_id": -1,
+      "image_url": "https://pix.as.criteo.net/img/img?c=1&h=400&i=10778--1670063402523121210&m=0&q=80&r=0&u=http%3A%2F%2Fdl-cron.lengow.com%2FFluxAccor%2FPhotos%2Ffiles%2F8780_ho_00_p_1024x768.jpg&v=2&w=400&s=xVeDD4NKWOu8FofUsDbQ-bIe",
+      "heading": "MERCURE BRISBANE KING GEORGE SQUARE",
+      "caption": "From $150 - Best price guaranteed !",
+      "clickthrough_url": "https://cat.hk.as.criteo.com/delivery/ckn.php?cppv=1&cpp=hZUz5XxnTEpVbHdZclhNVXN6L2FIWTgrR1dZUnQ1SDV3MmFhSC80TlVWamU2cDFQbFVFVWVmdncrbG5RcjlQMGFaeVBoM3ZObkxuRkUvbW1FS1JsU01zWTc4cHR5T0VzTjlSaFYzK0U4MG9mR3J1YlM4WmlYWDI3WnhzUWg0WlBUemZhcTNHSHJYeEtUb2kvcUhvRERRWmo5UExTWDg1VFIydDdWZEJ2c2hCU3JFQnREVUdnbTFoWjlWSzBqUys5eXdwWExqVzRmVHc0UW5IUWpZcmxaZ2tGajhMWFZMWnIrTHV6OU96VmdXQVUwY0VLNmZadkZhQ1VaaFZsUTNLZzlJWGlscXdJMGlGMVV5RklPOHl1WWN0Z1BPMzZoY3FyNDNQdFhrcVEzZy8zR2xLaEhoL0tUVTVFbVhVR01RRG1oeThkQ3w%3D&maxdest=http%3A%2F%2Fwww.accorhotels.com%2Flien_externe.svlt%3Fgoto%3Dfiche_hotel%26code_hotel%3D8780%26sourceid%3DCriteo-AU-8780%26merchantid%3DRT-FR018143-%26xtor%3DAL-187-%5BCriteo%5D-%5BAU%5D-%5B0%5D-%5B0%5D-"
+    }
+  ]
+});
