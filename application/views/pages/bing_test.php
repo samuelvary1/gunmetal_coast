c7d357b452fce7e16558de39b4ea9a8268ec21c5
strickswans <sswanson@triplelift.com>
2019-01-15 13:12:04 -0500

    Test page to experiemnt with Bing's test tag

---
+++ application/views/pages/bing_test.php
@@ -0,0 +1,19 @@
+<!-- test_page_2.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Generic Test Page 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+  	<div style="display: block;"><script src="//ib.3lift.com/ttj?inv_code=triplelift_bing_test"></script></div>
+  	<div id="bingnativead"></div>
+
+  </body>
+</html>
