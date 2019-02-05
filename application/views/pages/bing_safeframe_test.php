091cb30dfa8dfb72c9774564588877b30fea741b
samuelvary1 <svary@triplelift.com>
2019-02-05 16:01:08 -0500

    add bing test page

---
+++ application/views/pages/bing_safeframe_test.php
@@ -0,0 +1,32 @@
+<!-- bing_safeframe_test.php -->
+<!DOCTYPE HTML> 
+<html lang="en-us">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8"> 
+<title>Widgets Magazine</title> 
+<style type="text/css" media="screen"> 
+</style>
+<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+<script>
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
+</script>
+ 
+<script>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/22979003/bing_test_safeframe', [[300, 600], [970, 90], [300, 250]], 'div-gpt-ad-1549400122813-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+
+</head>
+
+
+
+<!-- /22979003/bing_test_safeframe -->
+<div id='div-gpt-ad-1549400122813-0'>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1549400122813-0'); });
+</script>
+</div>
