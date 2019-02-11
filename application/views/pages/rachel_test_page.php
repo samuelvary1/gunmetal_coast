757633c993db50bb01ee08881c0631abc718b65f
samuelvary1 <svary@triplelift.com>
2019-02-11 12:20:10 -0500

    set up test page for rachel

---
+++ application/views/pages/rachel_test_page.php
@@ -0,0 +1,21 @@
+<!-- rachel_test_page -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Generic Test Page 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+  	<div style="display: block;"><script src="//staging-ib.3lift.com/ttj?inv_code=rachel_test_placement_1"></script></div>
+  	<div style="display: block;"><script src="//staging-ib.3lift.com/ttj?inv_code=rachel_test_placement_2"></script></div>
+  	<div style="display: block;"><script src="//staging-ib.3lift.com/ttj?inv_code=rachel_test_placement_3"></script></div>
+  	
+
+  </body>
+</html>
