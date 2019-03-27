200bc1f7ae0f99b762fde28895bc56ba8c1a4bf2
samuelvary1 <svary@triplelift.com>
2019-03-27 14:02:41 -0400

    revert routing attempts

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'pages/view';
-$route['doubleclick'] = 'pages/doubleclick/view'
 $route['(:any)'] = 'pages/view/$1';
 
 
