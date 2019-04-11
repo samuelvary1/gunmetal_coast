2a81fcb4c56236a030999ece2734033d7598bbf0
samuelvary1 <svary@triplelift.com>
2019-04-11 11:14:17 -0400

    new test page for sandy

---
+++ application/views/pages/sandy_safeframe_test.php
@@ -0,0 +1,30 @@
+<!-- sandy_safeframe.php -->
+
+
+
+<html>
+<head>
+	<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+	<script>
+	  var googletag = googletag || {};
+	  googletag.cmd = googletag.cmd || [];
+	</script>
+	 
+	<script>
+	  googletag.cmd.push(function() {
+	    googletag.defineSlot('/22979003/sandy_safeframe_test', [[300, 250], [300, 600]], 'div-gpt-ad-1554995554897-0').addService(googletag.pubads());
+	    googletag.pubads().enableSingleRequest();
+	    googletag.enableServices();
+	  });
+	</script>
+</head>
+<body>
+	<!-- /22979003/sandy_safeframe_test -->
+<div id='div-gpt-ad-1554995554897-0'>
+	<script>
+	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1554995554897-0'); });
+	</script>
+</div>
+
+</body>
+</html>
---
+++ application/views/pages/travis_dot_com.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="http://4.bp.blogspot.com/-buEK3vlpyG8/VJ3V0xI0u7I/AAAAAAAA75I/-pLNWkz8d8A/s1600/babadook6.JPG" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_forest.jpg">
+    <img src="img_forest.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_lights.jpg">
+    <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_mountains.jpg">
+    <img src="img_mountains.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+</body>
+</html>
